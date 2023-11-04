d4429feac14904fafbab6f89a346ef693e99e686
Brandon Finger <bfinger.work@yahoo.com>
2023-11-03 19:03:35 -0500

    final 1

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Character from './Character'
 
-const urlPlanets = 'http://localhost:9009/api/planets'
+
 const urlPeople = 'http://localhost:9009/api/people'
+const urlPlanets = 'http://localhost:9009/api/planets'
 
 function App() {
+  const [people, setPeople] = useState([])
+
+
+  useEffect( () => {
+    axios
+    .get(urlPlanets)
+    .then(planetsResponse => {
+      const planets = planetsResponse.data;
+      console.log(planets)
+      axios
+      .get(urlPeople)
+      .then(peopleResponse => {
+        const people = peopleResponse.data;
+        const peopleWithPlanet = people.map(person => {
+          const planet = planets.find(planet => planet.id === person.homeworld);
+          return { ...person, planet };
+        });
+        setPeople(peopleWithPlanet);
+        console.log(peopleWithPlanet)
+      });
+    });
+  }, [])
   // ❗ Create state to hold the data from the API
   // ❗ Create effects to fetch the data and put it in state
   return (
@@ -13,6 +36,21 @@ function App() {
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {people.map(char => {
+        return <Character
+         data={char}
+         key={char.id}
+         fullName={char.name}
+         birth={char.birth_year}
+         mass={char.mass}
+         height={char.height}
+         gender={char.gender}
+         hair={char.hair_color}
+         eye={char.eye_color}
+         homeworld={char.planet.name}
+         planetDetails={char.planet}
+         />
+      })}   
     </div>
   )
 }
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,13 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 
-function Character() { // ❗ Add the props
+
+
+function Character({ data, fullName, birth, mass, height, gender, hair, eye, homeworld, planetDetails }) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+  const [showPlanetDetails, setShowPlanetDetails] = useState(false);
+  const [showCharacterDetails, setShowCharacterDetails] = useState(false)
+
+  const toggleCharacterDetails = () => {
+    setShowCharacterDetails(!showCharacterDetails)
+  }
+
+  const togglePlanetDetails = () => {
+    setShowPlanetDetails(!showPlanetDetails)
+  }
+
+  //console.log(data)
   return (
     <div>
       {/* Use the same markup with the same attributes as in the mock */}
+
+      {/* name is only thing showing until click*/}
+      <h3 onClick={togglePlanetDetails}>{fullName}</h3>
+
+      {/*this shows after name click and closes after clicked again */}
+      {/*in the info, homeworld is last and is a button that has the planet name */}
+      {
+        showPlanetDetails && (
+          <>
+            <p>Born: {birth}</p>
+            <p>Mass: {mass}</p>
+            <p>Height: {height}</p>
+            <p>Gender: {gender}</p>
+            <p>Hair Color: {hair}</p>
+            <p>Eye Color: {eye}</p>
+            <p onClick={toggleCharacterDetails}>more...</p>
+          </>
+        )
+      }
+
+      {/*this should render when the planet name button is clicked and closed when clicked again */}
+      {
+        showCharacterDetails && (
+          <>
+            <h3>{homeworld}</h3>
+            <p>Orbital Period: {planetDetails.orbital_period}</p>
+            <p>Climate: {planetDetails.climate}</p>
+            <p>Terrain: {planetDetails.terrain}</p>
+            <p>Population: {planetDetails.population}</p>
+          </>
+
+        )
+      }
+
     </div>
-  )
+  );
 }
 
 export default Character
